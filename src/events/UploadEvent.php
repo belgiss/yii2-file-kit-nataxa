3c3868c7d63ad23ecfce3521e54680c43e2dc96d
de57 <a.likhacheva@belgiss.by>
2023-07-31 13:25:40 +0300

    init: add package

---
+++ src/events/UploadEvent.php
@@ -0,0 +1,24 @@
+<?php
+namespace nataxa\filekit\events;
+use yii\base\Event;
+
+/**
+ * Class UploadEvent
+ * @package nataxa\filekit\events
+ * @author Eugene Terentev <eugene@terentev.net>
+ */
+class UploadEvent extends Event
+{
+    /**
+     * @var mixed
+     */
+    public $filesystem;
+    /**
+     * @var string
+     */
+    public $path;
+    /**
+     * @var \League\Flysystem\File|null
+     */
+    public $file;
+}
---
+++ src/filesystem/FilesystemBuilderInterface.php
@@ -0,0 +1,14 @@
+<?php
+namespace nataxa\filekit\filesystem;
+
+/**
+ * Interface FilesystemBuilderInterface
+ * @package nataxa\filekit\filesystem
+ */
+interface FilesystemBuilderInterface
+{
+    /**
+     * @return mixed
+     */
+    public function build();
+}
